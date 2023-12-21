05f08795b003459f319339f2972786c6bd3018ce
Cihanpy1 <yusufcihandiri@gmail.com>
2023-12-22 00:13:42 +0300

    python
    python öğrenirken yaptığım tüm projeleri burda paylaşiyorum

---
+++ ekrana-cikti-cikartmak/ekrana-cikti-cikartmak.py
@@ -0,0 +1,10 @@
+##tanımlanan değişkenler 
+isim = "yusuf"
+soy_isim = "diri"
+hobi = "Bilgisayar oyunları oynamak, bilardo"
+felsefe = "Hayat umursamayanlara güzeldir, boş yere kafanızı takmayın."
+##ekrana çıktı çıkartma
+print("İsim            :",isim)
+print("Soy isim        :",soy_isim)
+print("Hobileri        :",hobi)
+print("Hayat Felsefesi :",felsefe)
---
+++ python veri tipleri/temel-veri-tipler.py
@@ -0,0 +1,10 @@
+#int veri tipi
+a1 = 4    # a1 + b2 = 8.5
+#float veri tipi
+b1 = 4.5 
+c1 = a1 - b1
+#string veri tipi
+k1 = "cihan"
+k2 = "4"
+##ekrana yazdırma
+print(k1*a1)
