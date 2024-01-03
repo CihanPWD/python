2f88c422f2e4234ee1da3a6f814d372666a60ef2
CihanPWD <yusufcihandiri@gmail.com>
2024-01-03 14:00:02 +0300

    python

---
+++ kaçiş dizileri/kaçışdizeleri.py
@@ -0,0 +1,8 @@
+##kaçiş dizesi 
+print("\tyusuf diyor ki : \"dünya boştur\"\n\tgerçekten öyle")
+##windows sesi çikaran kaçiş dizesi
+print("\tses\a")
+##özel karakter çikaran kaçiş dizesi
+print("\tözel karakter\u6353")
+##dünya emojisi çikaran kaçiş dizesi
+print("\tdünya \U0001F30D")
